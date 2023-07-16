2753cf274d4b7bf2ef1d8cd9a5c1bf1526193ece
yubinCloud <yubin_SkyWalker@yeah.net>
2023-07-16 00:44:56 +0000

    lab2 将 struct 从 raft.go 移到 entity.go 中

---
+++ src/raft/entity.go
@@ -0,0 +1,98 @@
+package raft
+
+import (
+	"sync"
+
+	"6.5840/labrpc"
+)
+
+// as each Raft peer becomes aware that successive log entries are
+// committed, the peer should send an ApplyMsg to the service (or
+// tester) on the same server, via the applyCh passed to Make(). set
+// CommandValid to true to indicate that the ApplyMsg contains a newly
+// committed log entry.
+//
+// in part 2D you'll want to send other kinds of messages (e.g.,
+// snapshots) on the applyCh, but set CommandValid to false for these
+// other uses.
+type ApplyMsg struct {
+	CommandValid bool
+	Command      interface{}
+	CommandIndex int
+
+	// For 2D:
+	SnapshotValid bool
+	Snapshot      []byte
+	SnapshotTerm  int
+	SnapshotIndex int
+}
+
+type LogEntry struct {
+	Term    int
+	Command interface{}
+}
+
+// A Go object implementing a single Raft peer.
+type Raft struct {
+	mu        sync.Mutex          // Lock to protect shared access to this peer's state
+	peers     []*labrpc.ClientEnd // RPC end points of all peers
+	persister *Persister          // Object to hold this peer's persisted state
+	me        int                 // this peer's index into peers[]
+	dead      int32               // set by Kill()
+
+	// Your data here (2A, 2B, 2C).
+	// Look at the paper's Figure 2 for a description of what
+	// state a Raft server must maintain.
+	role        int  // Raft peer 的角色，0 - follower，1 - candidate，2 - leader
+	hasReceived bool // 每次 election timeout 期间是否有接收到消息
+	currentTerm int
+	votedFor    int
+	logs        []*LogEntry
+
+	voteCnt   int // 代表有几票了
+	recvVotes []bool
+
+	lastFrom int // 记录上一个 AppendEntries 来自于谁
+}
+
+// Raft 的 role
+const (
+	RAFT_ROLE_FOLLOWER  = 0
+	RAFT_ROLE_CANDIDATE = 1
+	RAFT_ROLE_LEADER    = 2
+)
+
+// *****************************************************
+// RPC struct
+// *****************************************************
+// example RequestVote RPC arguments structure.
+// field names must start with capital letters!
+type RequestVoteArgs struct {
+	// Your data here (2A, 2B).
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+// example RequestVote RPC reply structure.
+// field names must start with capital letters!
+type RequestVoteReply struct {
+	// Your data here (2A).
+	Term        int
+	VoteGranted bool
+}
+
+type AppendEntriesArgs struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      []LogEntry
+	LeaderCommit int
+}
+
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
